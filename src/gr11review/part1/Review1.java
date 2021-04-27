286d59c686b1ebb2a885004523e4e86c81eb296b
WearyWasTaken <67983460+WearyWasTaken@users.noreply.github.com>
2021-04-27 15:08:48 -0400

    Fixed the calculation system to account for February and January

--- src/gr11review/part1/Review1.java
+++ src/gr11review/part1/Review1.java
@@ -28,11 +28,26 @@ public class Review1{
         numDay = Integer.parseInt(key.readLine());
 
         //Assuming what is mentioned above, calculates the amount of days in the year
-        numTotalDays = (numMonth - 2) * 31 + numDay + 28;
+        if (numMonth < 2){
 
-        //Prints the total amount of days
-        System.out.println(numTotalDays);
+            numTotalDays = numDay;
 
+            System.out.println(numTotalDays);
+
+        }
+            else if (numMonth == 2){
+
+                numTotalDays = (numMonth - 1) * 31 + numDay;
+
+                System.out.println(numTotalDays);
+
+        }
+                 else{
+
+                    numTotalDays = (numMonth - 2) * 31 + numDay + 28;
+
+                    System.out.println(numTotalDays);
+                 }
 
     }
 }
