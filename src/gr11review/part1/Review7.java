0c7bc2784ea8ab12e3b12ea00bb9e7c40249dce6
WearyWasTaken <67983460+WearyWasTaken@users.noreply.github.com>
2021-04-30 09:19:34 -0400

    Added comments to Review7

--- src/gr11review/part1/Review7.java
+++ src/gr11review/part1/Review7.java
@@ -2,6 +2,13 @@ package gr11review.part1;
 
 import java.io.*;
 
+/**
+ * Takes an input for a sentence
+ * Counts the number of characters, spaces and "a"s in the sentence
+ * Prints a dash for every odd letter in the sentence starting from 1
+ * @author A. Wong
+ */
+
 public class Review7{
     public static void main(String[] args) throws IOException{
 
@@ -14,12 +21,14 @@ public class Review7{
 
         BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
 
+        //Takes input for the sentence
         System.out.println("Input a sentence: ");
         theSentence = key.readLine();
 
         char[] arrSentence = theSentence.toCharArray();
 
         for (int num1 = 0; num1 < arrSentence.length; num1++){
+            //Checks if there is a space or an a and adds to the value counter accordingly
             switch(arrSentence[num1]){
                 case ' ': 
                     numSpaces++;
@@ -31,6 +40,7 @@ public class Review7{
 
             }
 
+            //Checks if there is an odd letter and adds a dash to the oddCharacters string
             if ((num1 + 1) % 2 != 0){
 
                 oddCharacters += "-";
@@ -39,8 +49,10 @@ public class Review7{
             
         }
 
+        //Counts the number of characters in the string
         numCharacter = arrSentence.length - numSpaces;
 
+        //Prints the results
         System.out.println("There are " + numCharacter + " characters in the sentence.");
         System.out.println("There are " + numSpaces + " spaces in the sentence.");
         System.out.println("There are " + numA + " letter a in the sentence.");
