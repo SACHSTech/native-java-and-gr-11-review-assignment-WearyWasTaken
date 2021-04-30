5b7993b74c802c505cbe4cdc12ab0f216db362a5
WearyWasTaken <67983460+WearyWasTaken@users.noreply.github.com>
2021-04-30 13:40:15 -0400

    Finished Review6

--- src/gr11review/part1/Review6.java
+++ src/gr11review/part1/Review6.java
@@ -1,1 +1,55 @@
 package gr11review.part1;
+
+import java.io.*;
+import java.text.*;
+
+/**
+ * Takes the price of inputed numbers and adds them together
+ * to calculate tax and total
+ * Ends when 0 is inputed
+ * @author A. Wong
+ */
+
+public class Review6{
+    public static void main(String[] args) throws IOException{
+
+        double numPrice = 1;
+        double numSubTotal = 0;
+        double numTax;
+        double numTotal;
+        int numCounter = 1;
+
+        NumberFormat numberFormat = new DecimalFormat("##.##");
+
+        BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+
+        //Checks if inputed number is 0
+        //If it isn't, it ask for more input
+        //If it is, it finishes the calculations
+        while (numPrice != 0){
+
+                System.out.println("Enter the price for item " + numCounter);
+                numPrice = Double.parseDouble(key.readLine());
+
+                numSubTotal += numPrice;
+                numCounter++;
+
+        }
+
+        //Calculating for tax
+        numTax = numSubTotal * 0.13;
+
+        //Fixes taxes format
+        String format = numberFormat.format(numTax);
+
+        numTotal = numSubTotal + numTax;
+
+        //Prints the result
+        System.out.println("Subtotal: $" + numSubTotal);
+        System.out.println("Tax: $" + numTax);
+        System.out.println("Total: $" + numTotal);
+
+
+    }
+
+}
